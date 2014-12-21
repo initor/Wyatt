615e98e534fd902677a8b122f7f5496c2b4295b5
Yong WEN <heropotato1130@msn.com>
2014-12-21 17:43:46 -0500

    Add theme config

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -4,14 +4,15 @@
   // Declare the module of Angular
   var app = angular.module('WyattIn', ['ngMaterial', 'infinite-scroll']);
 
+
+  app.controller('PortraitsCtrlr', ['$http', function($http){
+
+  }]);
+  
   // Config theming
   app.config(function($mdThemingProvider) {
     $mdThemingProvider.theme('default')
     .primaryColor('pink')
     .accentColor('orange');
   });
-
-  app.controller('PortraitsCtrlr', ['$http', function($http){
-
-  }]);
 })();
