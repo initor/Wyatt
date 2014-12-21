fb68b2bc047f64f7fddec6e9bdd0b7a0612caeba
Yong WEN <heropotato1130@msn.com>
2014-12-21 16:59:04 -0500

    Try material layout

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,8 +1,9 @@
 // Main .js of Wyatt.in
-angular.module('buttonsDemo1', ['ngMaterial'])
-.controller('AppCtrl', function($scope) {
-  $scope.title1 = 'Button';
-  $scope.title4 = 'Warn';
-  $scope.isDisabled = true;
-  $scope.googleUrl = 'http://google.com';
-});
+
+(function(){
+  var app = angular.module('WyattIn', ['ngMaterial', 'infinite-scroll']);
+
+  app.controller('PortraitsCtrlr', ['$http', function($http){
+
+  }]);
+})();
