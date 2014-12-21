ba26f6cc20d921405c385798d245262232dea35a
Yong WEN <heropotato1130@msn.com>
2014-12-21 02:40:00 -0500

    1st commit

---
+++ JavaScript/main.js
@@ -0,0 +1,8 @@
+// Main .js of Wyatt.in
+angular.module('buttonsDemo1', ['ngMaterial'])
+.controller('AppCtrl', function($scope) {
+  $scope.title1 = 'Button';
+  $scope.title4 = 'Warn';
+  $scope.isDisabled = true;
+  $scope.googleUrl = 'http://google.com';
+});
