168b0f0297413aaf96979435e60e379989ac1b20
Yong WEN <heropotato1130@msn.com>
2014-12-21 18:17:05 -0500

    Test ngRepest with Infinit Loop

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -6,7 +6,7 @@
 
   app.controller('PortraitsCtrlr', ['$http', function($http){
     this.pictures = pics;
-    ctrl.loadItems = function(){
+    this.loadItems = function(){
       for(var i = 0; i<50; i++){
         var item = {};
         item.index = i;
