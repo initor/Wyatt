be29c9123733e9b8166fdb660f094e5074c67647
Yong WEN <heropotato1130@msn.com>
2014-12-21 17:39:37 -0500

    Add theme config

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,7 +1,15 @@
 // Main .js of Wyatt.in
 
 (function(){
+  // Declare the module of Angular
   var app = angular.module('WyattIn', ['ngMaterial', 'infinite-scroll']);
+
+  // Config theming
+  app.config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+    .primaryColor('pink')
+    .accentColor('orange');
+  });
 
   app.controller('PortraitsCtrlr', ['$http', function($http){
 
