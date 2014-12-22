e0b1be7448bd2ed8dbfbed12cc8afe6c7b248154
initor <dev@initial.io>
2014-12-22 11:51:29 -0500

    Fix one naming issue in main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -11,7 +11,7 @@
       var tempLoadPics = [];
       for(var i = 0; i<4; i++){
         var item = {};
-        item.index = ctl.pictures.length + 1;
+        item.index = pics.length + 1;
         item.content = new Date();
         tempLoadPics.push(item);
       }
