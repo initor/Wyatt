c8176e7494d46f164bd0568151d4a91021cfdfe1
initor <dev@initial.io>
2014-12-22 16:27:56 -0500

    Fonts changes

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -3,38 +3,36 @@
 (function(){
   // Declare the module of Angular
   var app = angular.module('WyattIn', ['ngMaterial', 'infinite-scroll']);
-
-  var pics = [];
+ 
   var indexOfPics = 0;
   var urlOfDemeImg = 'http://farm3.staticflickr.com/2807/9317135155_0a078a007e_k.jpg';
-  var captions = '"He does math problems out loud in his sleep."<br>"She collects vintage teacups."';
-
-  var initialLoadPics = [];
-
-  $http.get('http://api.flickr.com/services/feeds/groups_pool.gne?id=998875@N22&lang=en-us&format=json&jsoncallback=?')
-  .success(function(data, status, header, config){
-    angular.foreach(data.items, function(item, key){
-      var urlOfPic = (item.media.m).replace("_m.jpg", ".jpg");
-      var i = {};
-      item.index = indexOfPics;
-      indexOfPics++;
-      item.created = new Date();
-      item.caption = captions;
-      item.url = urlOfPic;
-
-      initialLoadPics.push(item);
-    });
-  }).error(function(data, status, header, config){
-
-  });
-
-  while(initialLoadPics.length){
-    pics.push(initialLoadPics.splice(0,2));
-  }
+  var captions = '"He does math problems out loud in his sleep."<br>"She collects vintage teacups."'; 
 
   app.controller('PortraitsCtrlr', ['$http', function($http){
     var ctl = this;
-    ctl.pictures = pics;
+    ctl.pictures = [];  
+    var initialLoadPics = [];
+
+    $http.get('http://api.flickr.com/services/feeds/groups_pool.gne?id=998875@N22&lang=en-us&format=json&jsoncallback=?')
+    .success(function(data, status, header, config){
+      angular.foreach(data.items, function(item, key){
+        var urlOfPic = (item.media.m).replace("_m.jpg", ".jpg");
+        var i = {};
+        item.index = indexOfPics;
+        indexOfPics++;
+        item.created = new Date();
+        item.caption = captions;
+        item.url = urlOfPic;
+
+        initialLoadPics.push(item);
+      });
+    }).error(function(data, status, header, config){
+
+    }); 
+
+    while(initialLoadPics.length){
+      ctl.pictures.push(initialLoadPics.splice(0,2));
+    }
 
     this.loadItems = function(){
       var tempLoadPics = [];
