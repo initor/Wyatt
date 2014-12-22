b98a3c69990f43cdee40e56218721d0671024f60
initor <dev@initial.io>
2014-12-22 16:59:06 -0500

    Fonts changes

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -15,7 +15,7 @@
 
     $http.get('https://api.flickr.com/services/rest/?&method=flickr.people.getPublicPhotos&api_key=eb6e83fd2b255ab9adfef687f4c18e63&user_id=30414895@N07&format=json&per_page=20')
     .success(function(data, status, header, config){
-      angular.foreach(data.items, function(item, key){
+      angular.forEach(data.items, function(item, key){
         var urlOfPic = (item.media.m);
         var i = {};
         item.index = indexOfPics;
