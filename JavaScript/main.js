8cd1f0c93bce8b86823c404353065127432e2ec5
initor <dev@initial.io>
2014-12-23 13:49:31 -0500

    Fix date Angular expression

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -9,10 +9,12 @@
   var captions = '"He does math problems out loud in his sleep."<br>"She collects vintage teacups."';
 
   app.controller('PortraitsCtrlr', ['$http', '$sce', function($http, $sce){
+
+    ctl.loadingLinear = true;
+    
     var ctl = this;
     ctl.pictures = [];
-    ctl.loadingLinear = true;
-    var originalLoads = [];
+    ctl.originalLoads = [];
 
     $http.get('https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=16c28cba59fbe3b8685bc2316a38a1a0&photoset_id=72157649472095227+&extras=description%2C+date_taken%2C+url_k%2C+url_o&format=json&nojsoncallback=1')
     .success(function(data, status, header, config){
@@ -21,27 +23,34 @@
         indexOfPics++;
         item.datetaken = new Date(item.datetaken.split(" ")[0]);
         item.description._content = $sce.trustAsHtml(item.description._content);
-        originalLoads.push(item);
+        ctl.originalLoads.push(item);
       });
  
       ctl.loadingLinear = false;
 
       for(var i = 0; i < 3; i++){
-        ctl.pictures.push(originalLoads.splice(0,2));
+        ctl.pictures.push(ctl.originalLoads.splice(0,2));
       } 
     }).error(function(data, status, header, config){
     });
 
     this.loadItems = function(){
-      if(originalLoads.length >= 6){
+
+      // Show loading linear
+      ctl.loadingLinear = true;
+
+      if(ctl.originalLoads.length >= 6){
         for(var i = 0; i < 3; i++){
-          ctl.pictures.push(originalLoads.splice(0,2));
+          ctl.pictures.push(ctl.originalLoads.splice(0,2));
         }
       }else{
         while(originalLoads.length){
-          ctl.pictures.push(originalLoads.splice(0,2));
+          ctl.pictures.push(ctl.originalLoads.splice(0,2));
         }
       }
+
+      // Hide loading linear
+      ctl.loadingLinear = false;
     };
   }]);
 })();
