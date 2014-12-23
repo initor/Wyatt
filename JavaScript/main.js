4a73a1e73dfcb1463aca6eadc19a9373788635e2
Yong WEN <heropotato1130@msn.com>
2014-12-22 23:55:53 -0500

    Integrate Flickr API

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -13,7 +13,7 @@
     ctl.pictures = [];
     var initialLoadPics = [];
 
-    $http.get('https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=595c6d500b09a6e37151e0d099eee03b&photoset_id=72157649472095227&extras=date_upload%2C+date_taken%2C+owner_name%2C+icon_server%2C+original_format%2C+last_update%2C+geo%2C+tags%2C+machine_tags%2C+url_sq%2C+url_t%2C+url_s%2C+url_m%2C+url_o&format=json&nojsoncallback=1&auth_token=72157649479403870-f6f68d8485975c03&api_sig=6336a189b366d9d7541fd067a02ccbcd')
+    $http.get('https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=595c6d500b09a6e37151e0d099eee03b&photoset_id=72157649472095227&extras=date_upload%2C+date_taken%2C+owner_name%2C+icon_server%2C+original_format%2C+geo%2C+tags%2C+machine_tags%2C+media%2C+url_k%2C+url_o&format=json&nojsoncallback=1&auth_token=72157649479403870-f6f68d8485975c03&api_sig=4ac490defa5a3d6b8a526e88fc9142ff')
     .success(function(data, status, header, config){
       angular.forEach(data.photoset.photo, function(item, key){
         item.index = indexOfPics;
