f700c00c3b7c61b62c381c1087f97563858003c1
initor <dev@initial.io>
2014-12-23 11:20:05 -0500

    Fix date Angular expression

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -14,7 +14,7 @@
     ctl.loadingLinear = true;
     var originalLoads = [];
 
-    $http.get('https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=595c6d500b09a6e37151e0d099eee03b&photoset_id=72157649472095227&extras=description%2C+date_upload%2C+date_taken%2C+owner_name%2C+icon_server%2C+original_format%2C+geo%2C+tags%2C+machine_tags%2C+media%2C+url_k%2C+url_o&format=json&nojsoncallback=1&auth_token=72157649479403870-f6f68d8485975c03&api_sig=cf620b094521e6fab8209a0c7b3c231d')
+    $http.get('https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=595c6d500b09a6e37151e0d099eee03b&photoset_id=72157649472095227&extras=description%2C+date_upload%2C+date_taken%2C+owner_name%2C+icon_server%2C+original_format%2C+geo%2C+tags%2C+machine_tags%2C+media%2C+url_k%2C+url_o&format=json&nojsoncallback=1&auth_token=72157649479403870-f6f68d8485975c03')
     .success(function(data, status, header, config){
       angular.forEach(data.photoset.photo, function(item, key){
         item.index = indexOfPics;
@@ -25,7 +25,7 @@
       });
  
       ctl.loadingLinear = false;
-      
+
       for(var i = 0; i < 3; i++){
         ctl.pictures.push(originalLoads.splice(0,2));
       } 
