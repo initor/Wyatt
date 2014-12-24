4d4dcdb5c363d50c7e1d47ac6529fa31e189ffd2
Yong WEN <heropotato1130@msn.com>
2014-12-24 15:17:35 -0500

    Refine location icon

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -15,6 +15,29 @@
     ctl.pictures = [];
     ctl.originalLoads = [];
 
+    var urlOfGeoApi = 'https://api.flickr.com/services/rest/?method=flickr.places.getInfo&api_key=eb6e83fd2b255ab9adfef687f4c18e63&place_id=ligPyJBUV7ObriEBmg&woe_id=55986868&format=json&nojsoncallback=1';
+
+    function generateGeoApiUrl(pId, wId){
+      return 'https://api.flickr.com/services/rest/?method=flickr.places.getInfo&api_key=eb6e83fd2b255ab9adfef687f4c18e63&place_id=' + pId + '&woe_id=' + wId + '&format=json&nojsoncallback=1';
+    }
+
+    function getGeoInfo(pId, wId){
+      var geoApiUrl = generateGeoApiUrl(pId, wId);
+      $http.get(geoApiUrl).success(function(data, status, header, config){
+        return data.locality._content;
+      }).error(function(data, status, header, config){
+        return status;
+      });
+    }
+
+    function setGeoInfo(array){
+      angualr.forEach(array, function(item, key){
+        if(item.place_id && item.woeid){
+          item.geoInfo = getGeoInfo(item.place_id, item.woeid);
+        }
+      });
+    }
+
     $http.get('https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=eb6e83fd2b255ab9adfef687f4c18e63&photoset_id=72157649472095227+&extras=description%2C+date_taken%2C+url_h%2C+url_o&format=json&nojsoncallback=1')
     .success(function(data, status, header, config){
       angular.forEach(data.photoset.photo, function(item, key){
@@ -32,8 +55,9 @@
       });
       ctl.loadingLinear = false;
 
-      for(var i = 0; i < 6; i++){
+      for(var i = 0; i < 3; i++){
         ctl.pictures.push(ctl.originalLoads.splice(0,2));
+        setGeoInfo(ctl.pictures);
       }
     }).error(function(data, status, header, config){
     });
@@ -42,16 +66,22 @@
 
       // Show loading linear
       ctl.loadingLinear = true;
+      var tempCollection = [];
 
       if(ctl.originalLoads.length >= 6){
-        for(var i = 0; i < 6; i++){
-          ctl.pictures.push(ctl.originalLoads.splice(0,2));
+        for(var i = 0; i < 3; i++){
+          tempCollection.push(ctl.originalLoads.splice(0,2));
         }
       }else{
         while(ctl.originalLoads.length){
-          ctl.pictures.push(ctl.originalLoads.splice(0,2));
+          tempCollection.push(ctl.originalLoads.splice(0,2));
         }
       }
+
+      setGeoInfo(tempCollection);
+      angular.forEach(tempCollection, function(pair, key){
+        ctl.pictures.push(pair);
+      });
 
       // Hide loading linear
       ctl.loadingLinear = false;
